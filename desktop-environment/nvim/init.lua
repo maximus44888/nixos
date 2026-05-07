e301efda00e766c51246d39e11d59ef9b411fd07
System administrator <56847436+maximus44888@users.noreply.github.com>
2026-05-07 04:47:41 +0200

    Hihglight yank in neovim

--- desktop-environment/nvim/init.lua
+++ desktop-environment/nvim/init.lua
@@ -24,3 +24,8 @@ vim.keymap.set('n', '<left>', '<cmd>echo "Use h"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j"<CR>')
+
+-- Highlight when yanking
+vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
+	callback = function() vim.hl.on_yank() end,
+})
